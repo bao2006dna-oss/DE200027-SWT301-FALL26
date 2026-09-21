2ac7042bbb7fc6b0cda09a208c9583541c5c4d2f
bao2006dna-oss <bao2006dna@gmail.com>
2026-09-21 14:05:47 +0700

    test(Multiply): Test parameterized cho phuong thuc multipy

--- LAB2/DemoCalculator/src/test/java/DinhGiaBao/CalculatorTest.java
+++ LAB2/DemoCalculator/src/test/java/DinhGiaBao/CalculatorTest.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 class CalculatorTest {
     private final Calculator calculator = new Calculator();
 
@@ -51,4 +52,19 @@ class CalculatorTest {
         );
         assertEquals("Cannot divide by zero", ex.getMessage());
     }
+    @ParameterizedTest(name = "Test {index} => {0} * {1} = {2}")
+    @CsvFileSource(resources = "/data.csv", numLinesToSkip = 1)
+    @DisplayName("multiply: kiểm thử với nhiều bộ dữ liệu từ CSV")
+
+
+    void multiply_VariousInputs_ReturnsProduct(int a, int b, int expected) {
+        int actual = calculator.multiply(a, b);
+        // Assert
+        assertEquals(expected, actual,
+                () -> a + " * " + b + " phải bằng " + expected);
+    }
+
+
+
+
 }
